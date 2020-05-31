6ed11c2542328825ad741552b890d5f5eb8829f7
dmcmcmahon714 <57333568+dmcmcmahon714@users.noreply.github.com>
2020-05-31 11:57:58 -0500

    Add view files and edit/destroy ability

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
 	end
 
 	def update
-		if @post.update
+		if @post.update(post_params)
 			redirect_to @post
 		else
 			render 'edit'
@@ -35,7 +35,7 @@ class PostsController < ApplicationController
 	def destroy
 		@post.destroy
 		redirect_to root_path
-	end
+	end 
 
 private 
 
